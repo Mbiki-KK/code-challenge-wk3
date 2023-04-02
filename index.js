be8542e1b1e14b98d338dc353148e24231694721
Mbiki-KK <wawerumbiki@gmail.com>
2023-04-03 00:59:04 +0300

    Second part done

--- index.js
+++ index.js
@@ -7,12 +7,48 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('http://localhost:3000/films/1')
       .then(response => response.json())
       .then(movie => displayMovieDetails(movie));
+      
   }
+  const filmsList = document.querySelector('#films');
+
+// Fetch movie data from the endpoint
+fetch('http://localhost:3000/films')
+  .then(response => response.json())
+  .then(data => {
+    // Remove the placeholder li element
+    const placeholder = document.querySelector('#films li');
+    
+
+    // Create a list item for each movie
+    data.forEach(movie => {
+      const li = document.createElement('li');
+      li.textContent = movie.title;
+      li.classList.add('film', 'item');
+      filmsList.appendChild(li);
+    });
+  })
+
+  const buyTickets = document.getElementById("buy-ticket");
+  const ticketAvailability = document.getElementById('ticket-availability');
+
+  let availableTickets = 10; // change this value to set the initial number of tickets available
+
+  buyTickets.addEventListener('click',() => {
+    if(availableTickets>0){
+      availableTickets--;
+      ticketAvailability.textContent=`available tickets = ${availableTickets}`;
+    } else {
+      alaert('Show sold out');
+    }
+  })
+
   
   function displayMovieDetails(movie) {
     const availableTickets = movie.capacity - movie.tickets_sold;
   
     const movieDetailsContainer = document.getElementById("movie-details");
+
+    
   
     const poster = document.createElement("img");
     poster.src = movie.poster;
